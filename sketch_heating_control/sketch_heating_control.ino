322a360dae1d84d541f3fa1e615aa45ebb4bcbb5
Michael Macherey <macherey@gmx.net>
2015-04-30 12:56:46 +0200

    clean up and improving sketch_heating_control.ino

--- sketch_heating_control/sketch_heating_control.ino
+++ sketch_heating_control/sketch_heating_control.ino
@@ -1,4 +1,4 @@
-  /*
+ /*
  * Heating Control for REMROB-Plattform
  *
  * Reading from S0-impulse Interface(EN 62053-31) of Powermeter, and writing pulses into EEPROM.
@@ -14,7 +14,6 @@
 //
 // The DallasTemperature library can do all this work for you!
 // http://milesburton.com/Dallas_Temperature_Control_Library
- 
  
 #include <EEPROMex.h>
 #include <OneWire.h>
@@ -34,14 +33,10 @@ float celsius;    // temperature of Sensor
 
 // ################# end DS18B20 Temperature Sensor ####################
 
-// ################ begin vars S0 Interface#############################
+// ################ begin vars S0 Interface #############################
 
-// these values are saved in EEPROM
-const byte EEPROM_ID = 0x99;                       // used to identify if valid data in EEPROM
-volatile double impulse = 0.000;                   // store impulse
+// constant used to identify EEPROM addresses
 
-// constants used to identify EEPROM addresses
-const int ID_ADDR = 5;                             // the EEPROM address used to store the ID
 const int addressOnEEPROM = 7;                     // the EEPROM address used to store the impulse 
 
 // Writing and retrieving double = 1010102.500,this are four bytes of EEPROM memory.
@@ -50,15 +45,14 @@ double outputOfEEPROM;
 // Interrupt at SZero(S0) Pin
 const int SZeroPin = 2;                           // initializing S0 Pin
 
-// stores first byte of EEPROM.read(ID_ADDR)
-byte id;
-
 //################ end S0 Interface ####################################
 
 void setup() {
+  
   // needed only for debugging purpose at Serial Monitor
   Serial.begin(9600);
   
+ 
   // ################# setup S0 Interface ################################
   
   pinMode(SZeroPin, INPUT);                            // defining SZeroPin as INPUT/OUTPUT
@@ -71,44 +65,12 @@ void setup() {
   
   pinMode(12,OUTPUT);          // defining pin 12 as OUTPUT for bridge communication
   
-  // this is for debugging purpose
+  // this is for debugging purpose with onboard LED
   // pinMode(13,OUTPUT);
   
   Bridge.begin();             // Starts Bridge, facilitating communication between the AVR and Linux processor
                               // begin() is a blocking function, this process takes approximately three seconds.
   // ################# end setup bridge ######################################
-  
-  // ####  initializing S0 interface and set EEPROM_ID #######################
-  
-  id = EEPROM.read(ID_ADDR);                     // read the first byte from the EEPROM
-  
-  // delay is for debugging purpose at Serial Monitor
-  //delay(5000);
-  
-  if(id == EEPROM_ID){
-    // here if the id value of EEPROM.read matches the value saved when writing EEPROM
-    
-    readImpulseFromEEPROM();
-    delay(500);
-/*    
-    Serial.print("Output of EEPROM if EEPROM_ID is set = " );
-    printDouble(outputOfEEPROM, 1000);
-    Serial.println("");
-*/   
-  }else {
-  // here if the ID is not found, so write the default data
-    
-  // Serial.print("ID is not found , setting ID!");
-    EEPROM.write(ID_ADDR, EEPROM_ID);
-    delay(200);
-    readImpulseFromEEPROM();
-    delay(200);
-    
-    // is for debugging purpose at Serial Monitor
-    // debugTraceElse();
-    
-  }
-  // ####  end initializing S0 interface #########################################
 }
 
 
@@ -126,6 +88,7 @@ void loop() {
   // Serial.print(String(celsius));
   Bridge.put("celsiusOutdoor", String((int)celsius));  // Send Outdoor Temperature 
   
+  Bridge.put("PowerMeterImpulse", String(0.001 + outputOfEEPROM,3));  // Send Powermeter impulse with 3 decimals
   // ######### end bridge ##########
   
   // is for debugging purpose of S0 interface at Serial Monitor
@@ -146,8 +109,8 @@ byte data[12];
 byte addr[8];
 
   if ( !ds.search(addr)) {
-//    Serial.println("No more addresses.");
-//    Serial.println();
+    Serial.println("No more addresses.");
+    Serial.println();
     ds.reset_search();
     delay(250);
     return;
@@ -164,19 +127,19 @@ byte addr[8];
   ds.select(addr);    
   ds.write(0xBE);         // Read Scratchpad
 
-// Serial.print("  Data = ");
-//  Serial.print(present, HEX);
-//  Serial.print(" ");
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
   for ( i = 0; i < 9; i++) {           // we need 9 bytes
     data[i] = ds.read();
-//    Serial.print(data[i], HEX);
-//    Serial.print(" ");
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
   }
   
-/*  Serial.print(" CRC=");
+  Serial.print(" CRC=");
   Serial.print(OneWire::crc8(data, 8), HEX);
   Serial.println();
-*/ 
+ 
     // Convert the data to actual temperature
   // because the result is a 16 bit signed integer, it should
   // be stored to an "int16_t" type, which is always 16 bits
@@ -211,32 +174,9 @@ void readImpulseFromEEPROM(){              // Read impulse from EEPROM
 
 void saveImpulseToEEPROM(){                  // Save impulse to EEPROM 
   // Attention: The EEPROM has an endurance of at least 100,000 write/erase cycles.
+    Serial.println("Interrupt");
    EEPROM.updateDouble(addressOnEEPROM, 0.001 + outputOfEEPROM);
-}
-
-void printDouble( double val, unsigned int precision){
-// this function is needed to print more than two decimial places e.g. 0.001 at the serial Monitor
-// prints val with number of decimal places determine by precision
-// NOTE: precision is 1 followed by the number of zeros for the desired number of decimial places
-// example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
-
-   Serial.print (int(val));  //prints the int part
-   Serial.print("."); // print the decimal point
-   unsigned int frac;
-   if(val >= 0){
-     frac = (val - int(val)) * precision;
-   }else{
-      frac = (int(val)- val ) * precision;
-   }
-   int frac1 = frac;
    
-   while( frac1 /= 10 )
-       precision /= 10;
-   precision /= 10;
-   while(  precision /= 10)
-       Serial.print("0");
-
-   Serial.println(frac,DEC) ;
 }
 
 void debugTrace(){
@@ -251,17 +191,10 @@ void debugTrace(){
     readImpulseFromEEPROM();
     delay(50);
     Serial.print("EEPROMv= " );
-    printDouble(outputOfEEPROM, 1000);
+    Serial.println(outputOfEEPROM,3);
     Serial.print("");
     Serial.println("##################");
  
   }
   // +++ end debugging block
-}
-
-void debugTraceElse(){
-  
-    Serial.print("Output of EEPROM = ");
-    printDouble(outputOfEEPROM, 1000);
-    Serial.print("");
 }
